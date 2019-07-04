8f0642e347db71950ad929e6be8c996567bd49d2
Osamu Mitsuhashi <osamu.mitsuhashi224@gmail.com>
2019-07-04 21:34:13 +0900

    🎉git emojiのテンプレート追加とngrokコマンド追加

--- linker.sh
+++ linker.sh
@@ -8,6 +8,7 @@ ln -sf ~/dotfiles/bash/bashrc ~/.bashrc
 ln -sf ~/dotfiles/ssh/config ~/.ssh/config
 ln -sf ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/git/gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/git/gitmessage ~/.gitmessage
 ln -sf ~/dotfiles/ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/xvimrc ~/.xvimrc
 ln -sf ~/dotfiles/config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
