bb51cf35e976b8b80bf84e68ca9a44f26160f20d
omitsuhashi <osamu.mitsuhashi224@gmail.com>
2021-09-30 23:10:13 +0900

    windowsでうまく動くように調整（ahkのやつはうまくできていないので自分で調整）
    詳細は以下参照
    https://github.com/karakaram/alt-ime-ahk

--- linker.bat
+++ linker.bat
@@ -1,2 +1,8 @@
 mklink %HOMEPATH%"\_vimrc" %HOMEPATH%"\dotfiles\vimrc.keymap"
+
+mklink %HOME%\.ideavimrc %HOME%\dotfiles\ideavimrc
+mklink %HOME%\.xvimrc %HOME%\dotfiles\xvimrc
+mklink /D %HOME%\.vim %HOME%\dotfiles\vim
+mklink "%HOME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" %HOME%\dotfiles\alt-ime-ahk.exe
+
 exit 0
