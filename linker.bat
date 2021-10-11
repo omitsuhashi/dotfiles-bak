3cee6e0dd129178c90aa9191bd6c454213f69864
omitsuhashi <osamu.mitsuhashi224@gmail.com>
2021-10-11 21:30:25 +0900

    add windows

--- linker.bat
+++ linker.bat
@@ -1,7 +1,7 @@
 mklink %HOMEPATH%"\_vimrc" %HOMEPATH%"\dotfiles\vimrc.keymap"
 
-mklink %HOME%\.ideavimrc %HOME%\dotfiles\ideavimrc
-mklink %HOME%\.xvimrc %HOME%\dotfiles\xvimrc
+mklink %HOME%\_ideavimrc %HOME%\dotfiles\ideavimrc
+mklink %HOME%\_xvimrc %HOME%\dotfiles\xvimrc
 mklink /D %HOME%\.vim %HOME%\dotfiles\vim
 mklink "%HOME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" %HOME%\dotfiles\alt-ime-ahk.exe
 
